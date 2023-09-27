1be31e52d01496741c5a81fd16ddabc051ee2b77
bryantosin1 <bryantosin1@gmail.com>
2023-09-27 13:54:23 +0700

    Prak2

---
+++ Praktikum2.dart
@@ -0,0 +1,21 @@
+void main() {
+var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+print(halogens);
+
+var names1 = <String>{};
+Set<String> names2 = {}; 
+var names3 = {};
+
+  print(names1); // Output: {}
+  print(names2); // Output: {}
+  print(names3); // Output: {}
+
+  names1.add("Nama: Bryan Tosin Saputro, NIM: 2141720220");
+
+  names2.addAll({"Nama: Bryan Tosin Saputro, NIM: 2141720220", "Nama: Bryan Tosin Saputro, NIM: 2141720220"});
+
+  print(names1); 
+  print(names2); 
+
+}
+
